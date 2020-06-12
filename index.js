677f2f5c16f7b44910a98b16f07e447ae87b462b
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-06-12 13:00:37 +0100

    Create templates

--- index.js
+++ index.js
@@ -24,11 +24,23 @@ const server = http.createServer((req, res) => {
 
   } else if (pathName === '/laptop' && id < laptopData.length) {
     res.writeHead(200, { 'Content-type': 'text/html'});
-    res.end(`This is the LAPTOP page for laptop ${id}!`);
 
+    fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+      const laptop = laptopData[id];
+      let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+          output = output.replace(/{%IMAGE%}/g, laptop.image);
+          output = output.replace(/{%PRICE%}/g, laptop.price);
+          output = output.replace(/{%SCREEN%}/g, laptop.screen);
+          output = output.replace(/{%CPU%}/g, laptop.cpu);
+          output = output.replace(/{%STORAGE%}/g, laptop.storage);
+          output = output.replace(/{%RAM%}/g, laptop.ram);
+          output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+      res.end(output);
+    });
+    
   } else {
     res.writeHead(404, { 'Content-type': 'text/html'});
-    res.end('URL was mot found on the server!')
+    res.end('URL was mot found on the server!');
   }
 
 });
@@ -36,3 +48,5 @@ const server = http.createServer((req, res) => {
 server.listen(1337, '127.0.0.1', () => {
   console.log('Listening for request now');
 });
+
+
